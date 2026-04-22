f2f9b5f43d2d53b289e81b30d2c11eba760096e4
codiesnutkiss-sudo <codiesnutkiss@gmail.com>
2026-04-22 16:13:48 -0400

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const { createWispServer } = require(“wisp-server-node”);
 
 const PORT = process.env.PORT || 3000;
 
-const MIME_TYPES = {
+const MIME = {
 “.html”: “text/html”,
 “.svg”:  “image/svg+xml”,
 “.js”:   “application/javascript”,
@@ -16,14 +16,14 @@ const MIME_TYPES = {
 “.png”:  “image/png”,
 “.ico”:  “image/x-icon”,
 “.txt”:  “text/plain”,
-“.json”: “application/json”,
+“.json”: “application/json”
 };
 
-const server = http.createServer((req, res) => {
-let urlPath = req.url.split(”?”)[0];
+const server = http.createServer(function(req, res) {
+var urlPath = req.url.split(”?”)[0];
 if (urlPath === “/” || urlPath === “”) urlPath = “/index.html”;
 
-const filePath = path.join(__dirname, urlPath);
+var filePath = path.join(__dirname, urlPath);
 
 if (!filePath.startsWith(__dirname)) {
 res.writeHead(403);
@@ -31,33 +31,31 @@ res.end(“Forbidden”);
 return;
 }
 
-fs.readFile(filePath, (err, data) => {
+fs.readFile(filePath, function(err, data) {
 if (err) {
 res.writeHead(404, { “Content-Type”: “text/plain” });
 res.end(“Not found: “ + urlPath);
 return;
 }
 
-
-const ext  = path.extname(filePath).toLowerCase();
-const mime = MIME_TYPES[ext] || "application/octet-stream";
+```
+var ext  = path.extname(filePath).toLowerCase();
+var mime = MIME[ext] || "application/octet-stream";
 
 res.writeHead(200, {
   "Content-Type":  mime,
   "Cache-Control": "no-cache",
   "Cross-Origin-Opener-Policy":   "same-origin",
-  "Cross-Origin-Embedder-Policy": "require-corp",
+  "Cross-Origin-Embedder-Policy": "require-corp"
 });
 res.end(data);
-
-  
+```
 
 });
 });
 
+createWispServer({ server: server, path: “/ws/” });
 
-createWispServer({ server, path: “/ws/” });
-
-server.listen(PORT, () => {
+server.listen(PORT, function() {
 console.log(“CineWeb running on port “ + PORT);
 });
